db826df79aab005f385e6111ba8de6611843b29d
s3igo <s3igo@users.noreply.github.com>
2023-04-12 08:46:16 +0000

    deploy: d13eff31cdaad486554de411b7355ba9126e368c

---
+++ _next/static/Otf9eS9PikwG9vFCvikYs/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/656-69c345a15afb4a96.js","static/chunks/pages/index-69868f3d883e2d8b.js"],"/_error":["static/chunks/pages/_error-dffd65023f03470c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/u3PtBvg1cY3iDgg-qm-3k/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-9d7e44ebe2e1a223.js"],"/_error":["static/chunks/pages/_error-dffd65023f03470c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
